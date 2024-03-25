464fffc89eb5f396cf68a1000c70b57e20495500
Dennis Njau <njaudennis30@gmail.com>
2024-03-25 15:29:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// Function continued lab
+
+function saturdayFun(activity = 'code'){
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(activity = 'go to the office'){
+    return `This monday, I will ${activity}`
+}
+
+function wrapAdjective(style){
+    return (adjective = "special"){
+        return `You are ${style}${adjective}${style}!`
+    }
+}
